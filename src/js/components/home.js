495b00908bf976290105af9436bde7092876a742
XoronEdge <ta.xit@yandex.com>
2019-06-01 14:02:14 +0500

    Add new about component

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import About from "./about";
 const Home = props => {
     return (
         <div class="d-flex justify-content-center">
             <h1>Hello Stop</h1>
+            <About />
         </div>
     );
 };
