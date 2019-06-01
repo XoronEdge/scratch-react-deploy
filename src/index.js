47047163f57e3c8008b8791b1c0c1d3585132a96
XoronEdge <ta.xit@yandex.com>
2019-06-01 13:56:25 +0500

    add src folder

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./js/components/home";
+function App() {
+    return <Home />;
+}
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/js/components/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = props => {
+    return (
+        <div class="d-flex justify-content-center">
+            <h1>Hello Stop</h1>
+        </div>
+    );
+};
+
+export default Home;
