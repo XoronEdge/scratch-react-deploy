0af89a755274b8317be4588999692a4c427a633d
XoronEdge <ta.xit@yandex.com>
2019-06-01 14:09:56 +0500

    check node envoirment

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import About from "./about";
+console.log(process.env.NODE_ENV)
 const Home = props => {
     return (
         <div class="d-flex justify-content-center">
